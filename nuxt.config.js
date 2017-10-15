3ab8f8bf47c0b2e46017ba6845f696c0b5cfe536
Toni <tonykurya@gmail.com>
2017-10-15 13:23:20 +0300

    Add project config file

---
+++ nuxt.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'temper-ui',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Add fade transition to every page of the applicationn
+  */
+  css: ['assets/main.css'],
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, ctx) {
+      if (ctx.dev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
